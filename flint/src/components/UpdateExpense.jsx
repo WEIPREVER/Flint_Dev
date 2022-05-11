5539a5bcfb074f73d9f388e2bcca939821bc6502
Vera E <97448213+ZenonCodes@users.noreply.github.com>
2022-05-11 04:26:32 -0400

    fixed issue with react hooks, remove package-lock.json

--- flint/src/components/UpdateExpense.jsx
+++ flint/src/components/UpdateExpense.jsx
@@ -1,24 +1,17 @@
-import React, {Component} from 'react';
+import React, {Component, useState} from 'react';
 import './UpdateExpense.css'
 
-class UpdateExpense extends Component{
-  constructor(props) {
-    super(props)
+const UpdateExpense = (props) => {
+  const [id, setId] = useState(props.params.id);
+  const [name, setName] = useState(props.params.name);
 
-    this.state = {
-      id: this.props.params.id,
-    }
-  }
 
-  render(){
   return (
-  <div className={""}>
-    <div className={"box-container container"}>
-      <h1 className={"updateExpense"}> Update Expense: "{this.props.params.name}" (ID: {this.props.params.id}) </h1>
-    </div>
-  </div>
+      <div className={""}>
+        <div className={"box-container container"}>
+          <h1 className={"updateExpense"}> Update Expense: "{name}" (ID: {id}) </h1>
+        </div>
+      </div>
   )
 }
-}
-
 export default UpdateExpense;
